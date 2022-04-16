b97dd2b15eb942a50bd427e0e8886f84832f42cb
Kravchenkostepan2005 <101333763+Kravchenkostepan2005@users.noreply.github.com>
2022-04-16 11:59:49 +0200

    Update def.py

--- def.py
+++ def.py
@@ -1,42 +1,27 @@
-class BankAccount:
-    def __init__(self, name, id, pin, balance=0):
-        self.__name = name
-        self.__id = id
-        self.__pin = pin
-        self.__balance = balance
-        self.list_bank = {}
-        self.name = name
+import time
 
-    def __print_private_account1_details(self):
-        return (self.__name, self.__id, self.__pin, self.__balance)
 
-    def check_private_details(self):
-        pin = input("Enter pin: ")
-        if self.__pin == int(pin):
-            return self.print_private_account1_details()
-        if self.__pin != int(pin):
-            return ('Wrong pin!')
+def showtime(func):
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result = func(*args, **kwargs)
+        t2 = time.time()
+        print(f'{func.__name__} executed in {t2 - t1} sec')
+        return result
+    return wrapper
 
-    def change_pin_code(self):
-        pin = input("Pin: ")
-        if self.__pin == int(pin):
-            self.__pin == pin
 
-account_1 = BankAccount('User', 4698346, 6056, balance = 10000)
-print(account_1._BankAccount__print_private_account1_details())
-print(account_1._BankAccount__name)
-def add_bank(self, bank_name, money):
-    self.list_bank[bank_name] = money
-def getname(self):
-    return self.name
-def info(self):
-    for key, val in self.list_bank.items():
-        print(f'bank name - {key}, balance - {val}')
-class ATM:
-    bank_name = "MonoBank"
-    def __init__(self, cash):
-        self.cash = cash
-    def withdraw(self, a):
+@showtime
+def original_func(a, b):
+    res = a ** b
+    print(res)
+    return res
+
+if __name__ == "__main__":
+    a = int(input("Enter a: "))
+    b = int(input("Enter b: "))
+    original_func(a, b)
+   def withdraw(self, a):
         cash = 5000000
         balance = 10000
         if a < cash:
@@ -48,28 +33,20 @@ class ATM:
     balance1 = balance + a
     account_2 = BankAccount('User', 4698346, 6056, balance1)
     print(account_2)
-    def writing(self, account_2):
-        with open("abc1.txt", "w") as f:
-            f.write("{a} amount of money has been withdrawn")
-            
-import datetime
-from functools import wraps
-def original_func(a, b):
-    res = a ** b
-    print(res)
-    return res
 
-if __name__ == "__main__":
-    a = int(input("Enter a: "))
-    b = int(input("Enter b: "))
-    original_func(a, b)
-def decorator(wraps):
-    @wraps
-    def wrapper():
-        a = int(input("Enter a: "))
-        b = int(input("Enter b: "))
-        startTime = datetime.datetime.now()
-        original_func(a, b)
-        durationTime = datetime.datetime.now() - startTime
-        print(durationTime)
+def showtime1(withdraw):
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result = withdraw(*args, **kwargs)
+        t2 = time.time()
+        print(f'{withdraw.__name__} executed in {t2 - t1} sec')
+        return result
     return wrapper
+
+
+@showtime1
+def original_func1():
+    with open("abc1.txt", "w") as f:
+        t1 = time.time()
+        t2 = time.time()
+        f.write(f"{original_func1.__name__} executed in {t2 - t1} sec")
